0bf91fea6a609e9c0373611d43aab8ddaf846612
ForYou-00 <141963767+ForYou-00@users.noreply.github.com>
2023-11-02 12:57:21 +0330

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,57 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "تو که میدونی وقتی هستی حال من بهتره...",
+    artist: "Amir Tataloo",
+    img: "music-1",
+    src: "music-1"
+
+  },
+  {
+    name: "کاش میشد بهت بگم عزیزم با من بمون ...",
+    artist: "Xaniar",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "هرچی دارم براتو دارم هواتو ...",
+    artist: "Armin",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "خونه من پر از تاریکیه توبیا پنجره ام سقف بکش ...",
+    artist: "Amir Tataloo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "راز شب راز نگااته ...",
+    artist: "Haamim",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "خنده داره نه دلم تنگه حتی واسه اخلاق زهرمارتم ...",
+    artist: "Ali Yasini",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
